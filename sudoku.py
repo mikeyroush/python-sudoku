58f25ba40e9b6595a71d0ab8463a6cdb34983f0b
Mikey Roush <mikey.roush97@gmail.com>
2020-05-20 12:31:39 -0500

    refactored code

---
+++ sudoku.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+class Sudoku:
+	
+	def __init__(self):
+		#test grid
+		self.grid = [[5,3,0,0,7,0,0,0,0],
+					[6,0,0,1,9,5,0,0,0],
+					[0,9,8,0,0,0,0,6,0],
+					[8,0,0,0,6,0,0,0,3],
+					[4,0,0,8,0,3,0,0,1],
+					[7,0,0,0,2,0,0,0,6],
+					[0,6,0,0,0,0,2,8,0],
+					[0,0,0,4,1,9,0,0,5],
+					[0,0,0,0,8,0,0,7,9]]
+		self.solutions = []
+			
+	#see if a number is possible at a given position
+	def possible(self, y, x, n):
+		#see if the number already exists in the row
+		if self.inRow(y, n):
+			return False
+		#see if the number already exists in the column
+		if self.inCol(x, n):
+			return False
+		#see if the number already exists in the square
+		if self.inSquare(y, x, n):
+			return False
+		#otherwise the number is possible
+		return True
+		
+	def inRow(self, y, n):
+		for i in range(9):
+			if self.grid[y][i] == n:
+				return True
+		
+	def inCol(self, x, n):
+		for i in range(9):
+			if self.grid[i][x] == n:
+				return True
+		
+	def inSquare(self, y, x, n):
+		#find the base position of a given square
+		x0 = (x//3) * 3
+		y0 = (y//3) * 3
+		for i in range(3):
+			for j in range(3):
+				if self.grid[y0 + i][x0 + j] == n:
+					return True
+	
+	#test all spots with all possible numbers and backtrack if need be
+	def solve(self):
+		for y in range(9):
+			for x in range(9):
+				if self.grid[y][x] == 0:
+					for n in range(1,10):
+						if self.possible(y,x,n):
+							self.grid[y][x] = n
+							self.solve()
+							self.grid[y][x] = 0
+					return #backtrack if nothing is possible
+		print(np.matrix(self.grid))
+		input("More?")
+		
+#Main
+sudoku = Sudoku()
+sudoku.solve()
